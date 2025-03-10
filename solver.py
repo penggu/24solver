5a79abbce6aa0477b871677cea4ae999b58bd91e
Peng Gu <penggu@gmail.com>
2025-03-10 14:52:44 -0700

    Doc: Enhance Solver class with detailed docstrings for methods and improve expression generation logic

--- solver.py
+++ solver.py
@@ -6,11 +6,6 @@ class Solver:
     """
     A class to solve the 24 game, where the goal is to find a way to 
     manipulate four numbers so that the end result is 24.
-
-    Attributes:
-    ----------
-    numbers : list
-        A list of four integers to be used in the game.
     """
 
     def __init__(self, numbers: list[int], target: int = 24):
@@ -41,14 +36,25 @@ class Solver:
         return interleaved
 
     def _generate_expressions(self, numbers, operators):
+        """
+        Generate all possible expressions by interleaving numbers and operators
+        and inserting parentheses in different patterns.
+
+        Args:
+            numbers (list): A list of numbers to be used in the expressions.
+            operators (list): A list of operators to be used in the expressions.
+
+        Returns:
+            list: A list of strings, each representing a possible expression.
+        """
         expressions = []
         # All possible ways to insert parentheses
         patterns = [
-            '(({} {} {}) {} {}) {} {}',
-            '({} {} ({} {} {})) {} {}',
-            '({} {} {}) {} ({} {} {})',
-            '{} {} (({} {} {}) {} {})',
-            '{} {} ({} {} ({} {} {}))'
+            '(({} {} {}) {} {}) {} {}', # ((a op b) op c) op d
+            '({} {} ({} {} {})) {} {}', # (a op (b op c)) op d
+            '({} {} {}) {} ({} {} {})', # (a op b) op (c op d)
+            '{} {} (({} {} {}) {} {})', # a op ((b op c) op d)
+            '{} {} ({} {} ({} {} {}))'  # a op (b op (c op d))
         ]
         for pattern in patterns:
             # Interleave numbers and operators
@@ -58,6 +64,16 @@ class Solver:
         return expressions
 
     def _evaluate_expression(self, expression):
+        """
+        Evaluates a mathematical expression and returns the result.
+
+        Args:
+            expression (str): The mathematical expression to evaluate.
+
+        Returns:
+            float: The result of the evaluated expression.
+            None: If a ZeroDivisionError occurs during evaluation.
+        """
         try:
             result = eval(expression)
             return result
@@ -65,14 +81,23 @@ class Solver:
             return None
 
     def _find_solutions_brute_force(self):
+        """
+        Finds all possible solutions to the 24 game using a brute force approach.
+        This method generates all permutations of the given numbers and all combinations
+        of the operators ('+', '-', '*', '/'). For each permutation of numbers and each 
+        combination of operators, it generates all possible expressions and evaluates them 
+        to check if they are close enough to the target value (24).
+        Returns:
+            list: A list of expressions (as strings) that evaluate to the target value.
+        """
         solutions = []
         operators = ['+', '-', '*', '/']
         # Generate all permutations of numbers and operators
-        num_permutations = permutations(self._numbers)
+        num_permutations = permutations(self._numbers) # This is an iterator
     
         for num_perm in num_permutations:
-            # Need to re-gen every time because op_combinations is an iterator
-            op_combinations = product(operators, repeat=3)
+            # BUG: in nested forloop, need to re-gen this iterator every time!!!
+            op_combinations = product(operators, repeat=3) # This is an iterator
             for op_comb in op_combinations:
                 expressions = self._generate_expressions(num_perm, op_comb)
                 for expr in expressions:
